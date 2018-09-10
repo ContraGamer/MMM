dfa822ccb785446a17668a0d386e5e0a2551d6a9
Alexander Gonzalez <agonzalez@stsg.com.co>
2018-09-10 09:50:34 -0500

    Se agrega Programa
    Se realiza progra en Python

---
+++ MMM.py
@@ -0,0 +1,68 @@
+a=int(input("Introduzca el número de entradas que desea hacer."))
+b=0
+d=[]
+f=0
+Mayor=0
+Menor=1
+
+
+while (b<a):
+    b=b+1
+    c=int(input("Valor:"))
+
+    if (Mayor < c):
+        Mayor=c
+    else:
+        Mayor=Mayor
+        
+    f=f+c
+    d.append(c)
+    e=len(d)
+
+def Media():
+    media=f/e
+    print("La media es: "+str(media))
+    
+def Mediana():
+
+    con=Mayor
+    Ord=[]
+    Des=d
+    Ord.insert(0,Mayor)
+    g=0
+    Mediana=0
+    Moda=[]
+    Inicio=0
+    while (e>g):
+
+        con=con-1
+        contador=d.count(con)
+        g=len(Ord)
+        if(contador>0):
+            if(contador>1):
+                Ord.insert(0,con)
+                Des.remove(con)
+                con=con+1
+            else:
+                Ord.insert(0,con)
+        
+        if(contador>Inicio):
+            
+            Moda.insert(0,con)
+        else:
+            Moda=Moda
+            
+    if(g%2==0):
+        g=(g/2)-1;
+        Mediana=((Ord[int(g)]+Ord[int(g)+1])/2)
+    else:
+        g=(g/2)-1
+        Mediana=(Ord[int(g+0.5)])
+        
+    print("El orden es: "+str(Ord))
+    print("La mediana: "+str(Mediana))
+    
+    print("La Moda es: "+str(Moda))
+
+Media()
+Mediana()
